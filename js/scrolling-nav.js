dacd67f1f52dd13a2f5959dde2e08df8f242cfc1
Havas Health Plus <tyler.hancock@havas.com>
2018-08-23 11:51:57 -0400

    init new sections

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -27,3 +27,13 @@
   });
 
 })(jQuery); // End of use strict
+
+$(document).ready(function () {
+  $('.imgh')
+    .mouseover(function () {
+    $(this).attr("src", "http://taditdash.files.wordpress.com/2014/01/tadit-dash.jpg");
+  })
+  .mouseout(function () {
+    $(this).attr("src", "http://taditdash.files.wordpress.com/2014/01/tadit-dash.jpg");
+  });
+});
