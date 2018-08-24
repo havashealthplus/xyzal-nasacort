f25ae084537b2275dadc5260ae6d20dae22da6f7
Havas Health Plus <tyler.hancock@havas.com>
2018-08-24 14:27:45 -0400

    final updates?

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -160,8 +160,15 @@ $(document).ready(function () {
   });
 });
 
+$( document ).ready(function() {
+    var heights = $(".card").map(function() {
+        return $(this).height();
+    }).get(),
 
+    maxHeight = Math.max.apply(null, heights);
 
+    $(".card").height(maxHeight);
+});
 
 
 
